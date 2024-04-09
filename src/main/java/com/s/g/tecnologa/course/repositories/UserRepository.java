7ff1ee595fb05815eed29df1c075133e14135da4
Leandro <leandrorecife@hotmail.com>
2024-04-09 14:11:12 -0400

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/s/g/tecnologa/course/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package com.s.g.tecnologa.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.s.g.tecnologa.course.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+	
+	
+	
+	
+
+}
